9ebaba7f1c836ce23978d8678b455a605a2f8663
amir-raul <s.amirraul.khan@progettomarconi.com>
2025-11-27 15:05:41 +0100

    task 2 comletato , aggiunto la logica delle mie relazionin e la bozza con la struttura della mia tabella

--- app/database/config.py
+++ app/database/config.py
@@ -7,7 +7,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-def get_db():            # Questa funzione serve a creare una sessione temporanea con il database per ogni richiesta HTTP, fornendola agli endpoint in modo sicuro e chiudendo automaticamente la connessione dopo l’uso.
+def get_db():            # Questa funzione serve a creare una sessione temporanea con il database per ogni richiesta HTTP,
     db = SessionLocal()  # fornendola agli endpoint in modo sicuro e chiudendo automaticamente la connessione dopo l’uso.
     try:
         yield db
